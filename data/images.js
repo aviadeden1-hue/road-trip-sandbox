86fdb7175e44dbaf6f3ac389e33a23b055ed495e
Aviad <aviad@roadtrip2026.local>
2026-05-10 23:45:52 -0400

    Fix Day 11 Golden Gate Bridge image

--- data/images.js
+++ data/images.js
@@ -17,7 +17,7 @@ window.IMAGE_DATA = {
   8:  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Bixby_Creek_Bridge%2C_California%2C_USA_-_May_2013.jpg/1280px-Bixby_Creek_Bridge%2C_California%2C_USA_-_May_2013.jpg",
   9:  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Seals_at_Piedras_Blancas_elephant_seal_rookery_2013_03.jpg/1280px-Seals_at_Piedras_Blancas_elephant_seal_rookery_2013_03.jpg",
   10: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/San_Francisco_from_Twin_Peaks_September_2013_panorama_5_edit.jpg/1280px-San_Francisco_from_Twin_Peaks_September_2013_panorama_5_edit.jpg",
-  11: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+  11: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Golden_Gate_Bridge_and_San_Francisco_skyline_from_Hawk_Hill_at_Blue_Hour_dllu.jpg/1280px-Golden_Gate_Bridge_and_San_Francisco_skyline_from_Hawk_Hill_at_Blue_Hour_dllu.jpg",
   12: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Sonoma_Valley.jpg/1280px-Sonoma_Valley.jpg",
   13: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Sonoma_wine_country.jpg/1280px-Sonoma_wine_country.jpg",
   14: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Along_Avenue_Of_The_Giants%2C_redwood_forest._%2810303011194%29.jpg/1280px-Along_Avenue_Of_The_Giants%2C_redwood_forest._%2810303011194%29.jpg",
